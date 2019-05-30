a18d91665a915fb93e8f505c2fc0d403ca6dffbf
Leonardo Miyagi <leonardomiyagi@gmail.com>
2019-05-30 17:12:41 -0400

    fixing some things | generating subtitles

--- app/src/main/java/br/com/leonardomiyagi/recyclerviewlinetogridtest/MainActivity.kt
+++ app/src/main/java/br/com/leonardomiyagi/recyclerviewlinetogridtest/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var adapter: DefaultAdapter
     private lateinit var layoutManager: GridLayoutManager
+    private lateinit var dividerItemDecoration: DividerItemDecoration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,9 +35,11 @@ class MainActivity : AppCompatActivity() {
                 if (layoutManager.spanCount == 1) {
                     layoutManager.spanCount = 3
                     item.setIcon(R.drawable.ic_grid_on)
+                    recyclerView.removeItemDecoration(dividerItemDecoration)
                 } else {
                     layoutManager.spanCount = 1
                     item.setIcon(R.drawable.ic_sort)
+                    recyclerView.addItemDecoration(dividerItemDecoration)
                 }
                 adapter.notifyItemRangeChanged(0, adapter.itemCount)
                 true
@@ -43,6 +49,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupRecyclerView() {
+        dividerItemDecoration = DividerItemDecoration(this, GridLayoutManager.VERTICAL)
         layoutManager = GridLayoutManager(this, 3)
 
         adapter = DefaultAdapter(layoutManager)
@@ -56,9 +63,46 @@ class MainActivity : AppCompatActivity() {
         for (i in 1..20) {
             items.add(TestItem(
                     "Test $i",
-                    "OAISJDOIAS FJOIASJF SAOIDJ OASIJD OASIJFOAIS FJoisjf OAISjf oASIjfO ASIjfOAIS fjOAISjf OAISfj OAISfj OASIfj AOSifjSAO Ifj AOISfjAOIsjfOAISJFOAIsj oiASJf OIASjf OIASfj AOISfj ASIf"
+                    generateSubtitle()
             ))
         }
         adapter.setItems(items)
+    }
+
+    private fun generateSubtitle(): String {
+        val words = ArrayList<String>()
+        words.add("something")
+        words.add("maybe")
+        words.add("yeah")
+        words.add("lets")
+        words.add("do")
+        words.add("this")
+        words.add("soy")
+        words.add("el")
+        words.add("fuego")
+        words.add("interview")
+        words.add("youtube")
+        words.add("google")
+        words.add("github")
+        words.add("mafia")
+        words.add("werewolf")
+        words.add("justkiddingparty")
+        words.add("mountain")
+        words.add("everest")
+        words.add("ground")
+        words.add("sky")
+        words.add("perplex")
+        var subtitle = ""
+        subtitle += "${words[Random().nextInt(words.size - 1)]}, "
+        subtitle += "${words[Random().nextInt(words.size - 1)]}, "
+        subtitle += "${words[Random().nextInt(words.size - 1)]}, "
+        subtitle += "${words[Random().nextInt(words.size - 1)]}, "
+        subtitle += "${words[Random().nextInt(words.size - 1)]}, "
+        for (i in 1..Random().nextInt(15)) {
+            subtitle += words[Random().nextInt(words.size - 1)]
+            subtitle += ", "
+        }
+        subtitle += words[Random().nextInt(words.size - 1)]
+        return subtitle
     }
 }
